7ebe288bf607330e318ea7bd4d1345abaa52bc3f
barbiero <pedrobarbiero@gmail.com>
2017-08-20 15:51:37 -0300

    Removido classes redundantes

--- opla-core/src/main/java/jmetal5/solution/PermutationSolution.java
+++
@@ -1,9 +0,0 @@
-package jmetal5.solution;
-
-/**
- * Interface representing permutation based solutions
- *
- * @author Antonio J. Nebro <antonio@lcc.uma.es>
- */
-public interface PermutationSolution<T> extends Solution<T> {
-}
